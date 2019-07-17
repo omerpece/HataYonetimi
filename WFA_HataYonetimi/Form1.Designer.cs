9d10be0daf02f84535ce054271f433cfacbce35f
muratvuranok <mrtvrnk@gmail.com>
2019-07-17 10:23:23 +0300

    Hata Yönetimi Ekranı Tasarlandı

--- WFA_HataYonetimi/Form1.Designer.cs
+++ WFA_HataYonetimi/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtGelenDeger = new System.Windows.Forms.TextBox();
+            this.btnOrnek1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtGelenDeger
+            // 
+            this.txtGelenDeger.Location = new System.Drawing.Point(12, 12);
+            this.txtGelenDeger.Name = "txtGelenDeger";
+            this.txtGelenDeger.Size = new System.Drawing.Size(318, 20);
+            this.txtGelenDeger.TabIndex = 0;
+            // 
+            // btnOrnek1
+            // 
+            this.btnOrnek1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek1.Location = new System.Drawing.Point(12, 39);
+            this.btnOrnek1.Name = "btnOrnek1";
+            this.btnOrnek1.Size = new System.Drawing.Size(318, 54);
+            this.btnOrnek1.TabIndex = 1;
+            this.btnOrnek1.Text = "Hata Yönetimsiz";
+            this.btnOrnek1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(554, 450);
+            this.Controls.Add(this.btnOrnek1);
+            this.Controls.Add(this.txtGelenDeger);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtGelenDeger;
+        private System.Windows.Forms.Button btnOrnek1;
     }
 }
 
