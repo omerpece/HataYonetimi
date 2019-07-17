33747707a372c0af97c2d5221e9cad78126c4e22
muratvuranok <mrtvrnk@gmail.com>
2019-07-17 10:25:53 +0300

    Hata Yönetimli Mesaj Buton Eklendi

--- WFA_HataYonetimi/Form1.Designer.cs
+++ WFA_HataYonetimi/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.txtGelenDeger = new System.Windows.Forms.TextBox();
             this.btnOrnek1 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtGelenDeger
@@ -61,11 +62,22 @@
             this.button1.Text = "Hata Yönetimli";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(318, 54);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Hata Yönetimli Mesaj";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(554, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnOrnek1);
             this.Controls.Add(this.txtGelenDeger);
@@ -81,6 +93,7 @@
         private System.Windows.Forms.TextBox txtGelenDeger;
         private System.Windows.Forms.Button btnOrnek1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
