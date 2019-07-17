5f81a35652c954f2d4b4c0cee40f85bd1e9a5475
muratvuranok <mrtvrnk@gmail.com>
2019-07-17 11:40:49 +0300

    hata yöntimi ders sonu

--- WFA_HataYonetimi/Form1.Designer.cs
+++ WFA_HataYonetimi/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.btnOrnek1 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.btnOrnek4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtGelenDeger
@@ -61,6 +62,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Hata Yönetimli";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // button2
             // 
@@ -71,12 +73,25 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "Hata Yönetimli Mesaj";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // btnOrnek4
+            // 
+            this.btnOrnek4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek4.Location = new System.Drawing.Point(12, 219);
+            this.btnOrnek4.Name = "btnOrnek4";
+            this.btnOrnek4.Size = new System.Drawing.Size(318, 54);
+            this.btnOrnek4.TabIndex = 4;
+            this.btnOrnek4.Text = "Hata Yönetimli Mesaj";
+            this.btnOrnek4.UseVisualStyleBackColor = true;
+            this.btnOrnek4.Click += new System.EventHandler(this.BtnOrnek4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(554, 450);
+            this.ClientSize = new System.Drawing.Size(341, 285);
+            this.Controls.Add(this.btnOrnek4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnOrnek1);
@@ -94,6 +109,7 @@
         private System.Windows.Forms.Button btnOrnek1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnOrnek4;
     }
 }
 
--- WFA_HataYonetimi/Form1.cs
+++ WFA_HataYonetimi/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace WFA_HataYonetimi
@@ -19,7 +13,69 @@ namespace WFA_HataYonetimi
 
         private void BtnOrnek1_Click(object sender, EventArgs e)
         {
+            int telefon = Convert.ToInt32(txtGelenDeger.Text);
+            MessageBox.Show("Tebrikler Telefon Numarasını Düzgün Girebildin!");
+        }
 
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int telefon = Convert.ToInt32(txtGelenDeger.Text);
+                MessageBox.Show("Tebrikler Telefon Numarasını Düzgün Girebildin!");
+            }
+            catch
+            {
+                MessageBox.Show("hata ile karşılaştık, lütfen tekrar deneyiniz!");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int telefon = Convert.ToInt32(txtGelenDeger.Text);
+                MessageBox.Show("Tebrikler Telefon Numarasını Düzgün Girebildin!");
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show("hata ile karşılaştık, lütfen tekrar deneyiniz!");
+                MessageBox.Show(hata.Message);
+            }
+        }
+
+        private void BtnOrnek4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int telefon = Convert.ToInt32(txtGelenDeger.Text);
+                MessageBox.Show("Tebrikler Telefon Numarasını Düzgün Girebildin!");
+
+                //int sayi = 0;
+                //int sonuc = 10 / sayi;
+            }
+            catch (DivideByZeroException ex)
+            {
+                // sıfıra bölünme hatası
+            }
+            catch (FormatException ex)
+            {
+                // yanlış formatta veri tipi gönderme
+            }
+            catch (OverflowException ex)
+            {
+                // bir nesnenin alabileceğinden fazla değer gönderme
+            }
+            catch (SqlException ex)
+            {
+                // Sql işlemleri sırasında bir hata ile karşılaşılır ise,  ADO.NET kullanıyorsanız.
+            }
+            catch (Exception hata)
+            {
+                // Yukarıda yer alan hatalar haricinde bir durum ile karşılaşılır ise
+                MessageBox.Show("hata ile karşılaştık, lütfen tekrar deneyiniz!");
+                MessageBox.Show(hata.Message);
+            }
         }
     }
 }
